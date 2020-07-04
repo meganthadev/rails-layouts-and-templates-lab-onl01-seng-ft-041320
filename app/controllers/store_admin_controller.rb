bad2a057598e9a45550a9418f1e0d333db16a5ee
Megan Horsfield <meganthadev@gmail.com>
2020-07-04 08:07:30 +0000

    Automatically backed up by Learn

--- app/controllers/store_admin_controller.rb
+++ app/controllers/store_admin_controller.rb
@@ -1,16 +1,15 @@
 class StoreAdminController < ApplicationController
-   layout "admin"
-   
+  layout "admin"
+
   def home
   end
-  
-  def orders 
+
+  def orders
     render :layout => "order_administration"
-  end 
-  
+  end
+
   def invoice
     render :layout => false
-  end   
-  
-  
-end   
+  end
+
+end
